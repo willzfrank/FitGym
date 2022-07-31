d14d3a1664739d173d843fb7c053c55e05b61d63
willzfrank <willzudoh4@gmail.com>
2022-07-31 21:14:01 +0100

    updated error

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,115 +1,67 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import logo from '../assets/images/logo-main.jpg';
-// {
-//
-// }
-
-
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  //   function bgImage() {
+  //   if (item === 'all') {
+  // 	return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />
+  //   )
+  // } else if(item === 'arms') {
+  // 		return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />)
+  // }else if(item === 'back') {
+  //   	return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />)
+  // } else if(item === 'chest') {
+  //   	return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />)
+  // }else if(item === 'legs') {
+  // 	return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />)
+  // }else if(item === 'shoulders') {
+  //   	return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />)
+  // } else if(item === 'abs') {
+  //   	return(
+  //     <img
+  //         className="hero-profile-img"
+  //         src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
+  //         alt=""
+  //       />)
 
-  function bgImage() {
-  if (item === 'all') {
-	return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />
-  )
-} else if(item === 'arms') {
-		return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />)
-}else if(item === 'back') {
-  	return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />)
-} else if(item === 'chest') {
-  	return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />)
-}else if(item === 'legs') {
-	return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />)
-}else if(item === 'shoulders') {
-  	return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />)
-} else if(item === 'abs') {
-  	return(
-    <img
-        className="hero-profile-img"
-        src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg"
-        alt=""
-      />)
-  
-} 
-  }
-
-  // <Stack
-  //   type="button"
-  //   alignItems="center"
-  //   justifyContent="center"
-  //   className="bodyPart-card"
-  //   sx={
-  //     bodyPart === item
-  //       ? {
-  //           borderTop: '4px solid #FF2625',
-  //           background: '#fff',
-  //           borderBottomLeftRadius: '20px',
-  //           width: '270px',
-  //           height: '282px',
-  //           cursor: 'pointer',
-  //           gap: '47px',
-  //         }
-  //       : {
-  //           background: '#fff',
-  //           borderBottomLeftRadius: '20px',
-  //           width: '270px',
-  //           height: '282px',
-  //           cursor: 'pointer',
-  //           gap: '47px',
-  //         }
+  // }
   //   }
-  //   onClick={() => {
-  //     setBodyPart(item);
-  //     window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
-  //   }}
-  // >
-  //   <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-  //   <Typography
-  //     fontSize="24px"
-  //     fontWeight="bold"
-  //     fontFamily="Alegreya"
-  //     color="#3A1212"
-  //     textTransform="capitalize"
-  //   >
-  //
-  //     {item}
-  //   </Typography>
-  // </Stack>
 
   <Stack>
     <div className="first hero">
-      {bgImage()}
+      {/* {bgImage()} */}
+      {/* Image ought to be here */}
       <div className="hero-description-bk" />
       <div className="hero-logo">
         <img src={logo} alt="" />
